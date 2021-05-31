78b30647afcede607c394bb1900578db1fd12c9a
yongqi <77aimiao@gmail.com>
2021-05-31 19:00:30 +0800

    feat: fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,17 +2,26 @@
   <h1>{{ msg }}</h1>
 
   <label>
-    <input type="checkbox" v-model="useScriptSetup" /> Use
+    <input
+      v-model="useScriptSetup"
+      type="checkbox"
+    > Use
     <code>&lt;script setup&gt;</code>
   </label>
   <label>
-    <input type="checkbox" v-model="useTsPlugin" /> Provide types for
+    <input
+      v-model="useTsPlugin"
+      type="checkbox"
+    > Provide types for
     <code>*.vue</code> imports
   </label>
 
   <p>
     Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    <a
+      href="https://code.visualstudio.com/"
+      target="_blank"
+    >VSCode</a>
     +
     <template v-if="!useScriptSetup">
       <a
@@ -26,28 +35,39 @@
       >Vue DX</a>
     </template>
     <template v-else>
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+      <a
+        href="https://github.com/johnsoncodehk/volar"
+        target="_blank"
+      >Volar</a>
     </template>
   </p>
   <p v-if="useTsPlugin">
     tsconfig setup:
-    <br />1. Install and add
+    <br>1. Install and add
     <code>@vuedx/typescript-plugin-vue</code> to tsconfig plugins
-    <br />2. Delete <code>src/shims-vue.d.ts</code>
-    <br />3. Open
+    <br>2. Delete <code>src/shims-vue.d.ts</code>
+    <br>3. Open
     <code>src/main.ts</code> in VSCode
-    <br />4. Open VSCode command input
-    <br />5. Search and run "Select TypeScript version" -> "Use workspace version"
+    <br>4. Open VSCode command input
+    <br>5. Search and run "Select TypeScript version" -> "Use workspace version"
   </p>
-  <button @click="count++">count is: {{ count }}</button>
+  <button @click="count++">
+    count is: {{ count }}
+  </button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+    <a
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank"
+    >Vite Docs</a> |
+    <a
+      href="https://v3.vuejs.org/"
+      target="_blank"
+    >Vue 3 Docs</a>
   </p>
 </template>
 
