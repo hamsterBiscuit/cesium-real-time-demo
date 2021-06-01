976fa9e6f38faa7122b4f07020cb4d3d06dd22d4
yongqi <77aimiao@gmail.com>
2021-06-01 23:49:49 +0800

    feat: update models and datas

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,16 @@
         10000
       ),
       model: {
-        uri: 'public/model/j20.gltf',
+        // uri: 'public/model/j20.gltf', // 歼20
+        // uri: 'public/model/BD1H.gltf', // 卫星1
+        // uri: 'public/model/czld.gltf', // 雷达车
+        // uri: 'public/model/DMLD.gltf', // 地面雷达
+        // uri: 'public/model/GALILEO.gltf', // 卫星2
+        // uri: 'public/model/GPS.gltf', // GPS
+        // uri: 'public/model/ZP.gltf', // 帐篷
+        // uri: 'public/model/KJ2000.glb', // 巡航机
+        // uri: 'public/model/missile.glb', // 导弹
+        uri: 'public/model/J15.glb', // 导弹
         minimumPixelSize: 128,
       },
     })
