70fa1c4de22eb51ac90dd97fdfd8ed829779a5e4
yongqi <77aimiao@gmail.com>
2021-06-08 18:27:36 +0800

    feat: update modle

--- src/hooks/cesium.ts
+++ src/hooks/cesium.ts
@@ -118,7 +118,6 @@ export const changeCameraHeight = (viewer: Ref<Cesium.Viewer>): void => {
   let isHigh = true
   handler.setInputAction(function (wheelment) {
     const height = viewer.value.camera.positionCartographic.height
-    console.log(height)
     if (height > benchmark && !isHigh) {
       isHigh = !isHigh
       changeEntityModel(viewer, 'img')
@@ -231,7 +230,7 @@ export const renderEntity = (
       uri: current.url,
       scale: current.scale,
       maximumScale: current.maximumScale,
-      minimumPixelSize: 32,
+      minimumPixelSize: 64,
       show: false,
     },
     billboard: {
@@ -562,6 +561,127 @@ export const renderEntity = (
         heading,
         materialData.parabolaHeight
       )
+    })
+
+    // test
+    // Define the trajectory dynamic line texture of the line -->
+    function PolylineTrailLinkMaterialProperty(color, duration) {
+      this._definitionChanged = new Cesium.Event()
+      this._color = undefined
+      this._colorSubscription = undefined
+      this.color = color
+      this.duration = duration
+      this._time = new Date().getTime()
+    }
+    Object.defineProperties(PolylineTrailLinkMaterialProperty.prototype, {
+      isConstant: {
+        get: function () {
+          return false
+        },
+      },
+      definitionChanged: {
+        get: function () {
+          return this._definitionChanged
+        },
+      },
+      color: Cesium.createPropertyDescriptor('color'),
+    })
+    PolylineTrailLinkMaterialProperty.prototype.getType = function (time) {
+      return 'PolylineTrailLink'
+    }
+    PolylineTrailLinkMaterialProperty.prototype.getValue = function (
+      time,
+      result
+    ) {
+      if (!Cesium.defined(result)) {
+        result = {}
+      }
+      result.color = Cesium.Property.getValueOrClonedDefault(
+        this._color,
+        time,
+        Cesium.Color.WHITE,
+        result.color
+      )
+      result.image = Cesium.Material.PolylineTrailLinkImage
+      result.time =
+        ((new Date().getTime() - this._time) % this.duration) / this.duration
+      return result
+    }
+    PolylineTrailLinkMaterialProperty.prototype.equals = function (other) {
+      return (
+        this === other ||
+        (other instanceof PolylineTrailLinkMaterialProperty &&
+          Property.equals(this._color, other._color))
+      )
+    }
+    // Mount the relevant flow line texture on Material, which can be encapsulated according to your needs
+    Cesium.Material.PolylineTrailLinkType = 'PolylineTrailLink'
+    Cesium.Material.PolylineTrailLinkImage =
+      Cesium.buildModuleUrl('/img/line.jpg')
+    // Define the core part of the shader source code
+    //   Cesium.Material.PolylineTrailLinkSource =
+    //     'czm_material czm_getMaterial(czm_materialInput materialInput)\n\
+    // {\n\
+    //     czm_material material = czm_getDefaultMaterial(materialInput);\n\
+    //     vec2 st = materialInput.st;\n\
+    //     vec4 colorImage = texture2D(image, vec2(fract(st.s - time), st.t));\n\
+    //     material.alpha = colorImage.a;\n\
+    //     material.diffuse = colorImage.rgb;\n\
+    //     return material;\n\
+    // }'
+    Cesium.Material.PolylineTrailLinkSource =
+      'czm_material czm_getMaterial(czm_materialInput materialInput)\n\
+                                                      {\n\
+                                                           czm_material material = czm_getDefaultMaterial(materialInput);\n\
+                                                           vec2 st = materialInput.st;\n\
+                                                           vec4 colorImage = texture2D(image, vec2(fract(st.s - time), st.t));\n\
+                                                           material.alpha = colorImage.a * color.a;\n\
+                                                           material.diffuse = (colorImage.rgb+color.rgb)/2.0;\n\
+                                                           return material;\n\
+                                                       }'
+    Cesium.Material._materialCache.addMaterial(
+      Cesium.Material.PolylineTrailLinkType,
+      {
+        fabric: {
+          type: Cesium.Material.PolylineTrailLinkType,
+          uniforms: {
+            color: new Cesium.Color(1.0, 0.0, 0.0, 0.5),
+            image: Cesium.Material.PolylineTrailLinkImage,
+            time: 0,
+          },
+          source: Cesium.Material.PolylineTrailLinkSource,
+        },
+        translucent: function (material) {
+          return true
+        },
+      }
+    )
+    // test part
+    // Calculate the rounded point at the inflection point
+    function computeCircle(radius) {
+      var positions = []
+      for (var i = 0; i < 360; i++) {
+        var radians = Cesium.Math.toRadians(i)
+        positions.push(
+          new Cesium.Cartesian2(
+            radius * Math.cos(radians),
+            radius * Math.sin(radians)
+          )
+        )
+      }
+      return positions
+    }
+    // Add a polyline tube
+    var polylineTrail = viewer.entities.add({
+      name: 'PolylineTrail',
+      polyline: {
+        positions: Cesium.Cartesian3.fromDegreesArrayHeights([
+          108.15890229003162, 33.73081694953842, 0, 109.097528, 34.1095, 0,
+          108.949322, 34.130466, 0.0,
+        ]),
+        width: 2,
+        material: new PolylineTrailLinkMaterialProperty(Cesium.Color.WHITE, 1000),
+      },
     })
   }
 }
